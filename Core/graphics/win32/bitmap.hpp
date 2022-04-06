8966156814a7f57b0d75639dbe2fbbc3be72df4e
Nianyi Wang <Wangnianyi2001@outlook.com>
2022-04-06 16:51:59 +0800

    Draw line

--- Core/graphics/win32/bitmap.hpp
+++ Core/graphics/win32/bitmap.hpp
@@ -10,12 +10,13 @@ namespace Graphics {
 	namespace Win32 {
 		class BitmapContext : public Context<HDC> {
 		protected:
-			int width, height;
-			HDC target, hDC;
+			HDC target;
 			HBITMAP hBm = NULL;
 			BYTE *data = nullptr;
 			virtual void Dispose() override;
 		public:
+			int width, height;
+			HDC hDC;
 			virtual ~BitmapContext() override;
 			virtual void SetTarget(HDC const &target) override;
 			virtual void Render() override = 0;
--- Core/main.cpp
+++ Core/main.cpp
@@ -1,23 +1,73 @@
 #include "graphics/win32/bitmap.hpp"
+#include <vector>
 
 using namespace Graphics::Win32;
+
+struct Vec2 {
+	float x, y;
+	float mod() {
+		return sqrtf(x * x + y * y);
+	}
+	Vec2 operator+(Vec2 pos) {
+		return Vec2{ x + pos.x, y + pos.y };
+	}
+	Vec2 operator*(float scale) {
+		return Vec2{ x * scale, y * scale };
+	}
+	Vec2 operator-(Vec2 pos) {
+		return operator+(pos * -1);
+	}
+	Vec2 unify() {
+		return operator*(1 / mod());
+	}
+};
+
+struct Line {
+	Vec2 begin, end;
+	Vec2 vec() {
+		return end - begin;
+	}
+	float len() {
+		return vec().mod();
+	}
+};
 
 class DrawingContext : public BitmapContext {
 	static constexpr inline BLENDFUNCTION blend_function = {
 		AC_SRC_OVER, 0, 255, AC_SRC_ALPHA
 	};
 public:
+	void DrawPixel(Vec2 pos) {
+		Rectangle(hDC, pos.x, pos.y, pos.x + 2, pos.y + 2);
+		int a = GetLastError();
+	}
+
+	void DrawLine(Line line) {
+		HGDIOBJ old_brush = SelectObject(hDC, GetStockObject(WHITE_BRUSH));
+		HGDIOBJ old_pen = SelectObject(hDC, GetStockObject(NULL_PEN));
+		float len = line.len();
+		int step_count = ceilf(len);
+		float unit_len = len / step_count;
+		Vec2 unit = line.vec().unify() * unit_len;
+		for(int i = 0; i < step_count; ++i) {
+			Vec2 pos = line.begin + unit * i;
+			DrawPixel(pos);
+		}
+		DrawPixel(line.end);
+		SelectObject(hDC, old_brush);
+		SelectObject(hDC, old_pen);
+	}
+
 	virtual void Render() override {
-		SelectObject(hDC, GetStockObject(BLACK_BRUSH));
-		Rectangle(hDC, 0, 0, width, height);
-		SelectObject(hDC, GetStockObject(WHITE_BRUSH));
-		Ellipse(hDC, 0, 0, 100, 200);
 		//AlphaBlend(target, 0, 0, width, height, hDC, 0, 0, width, height, blend_function);
 		BitBlt(target, 0, 0, width, height, hDC, 0, 0, SRCCOPY);
 	}
 };
 
-BitmapContext *context;
+DrawingContext *context;
+bool first = false;
+Vec2 mouse_pos, first_point;
+std::vector<Line> lines;
 
 LRESULT WINAPI MsgProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
 	switch(message) {
@@ -25,7 +75,26 @@ LRESULT WINAPI MsgProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
 		context->SetTarget(GetDC(hWnd));
 		break;
 	case WM_PAINT:
+		SelectObject(context->hDC, GetStockObject(BLACK_BRUSH));
+		Rectangle(context->hDC, 0, 0, context->width, context->height);
+		for(Line line : lines)
+			context->DrawLine(line);
+		if(first)
+			context->DrawLine({ first_point, mouse_pos });
 		context->Render();
+		ValidateRect(hWnd, NULL);
+		break;
+	case WM_LBUTTONDOWN:
+		if(first)
+			lines.push_back({ first_point, mouse_pos });
+		else
+			first_point = mouse_pos;
+		first = !first;
+		break;
+	case WM_MOUSEMOVE:
+		mouse_pos = { (float)LOWORD(lParam), (float)HIWORD(lParam) };
+		if(first)
+			InvalidateRect(hWnd, NULL, false);
 		break;
 	case WM_DESTROY:
 		delete context;

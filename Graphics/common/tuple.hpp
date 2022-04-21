8c12110fe964a603f357bee06186c9c8199c60e3
Nianyi Wang <Wangnianyi2001@outlook.com>
2022-04-21 21:56:21 +0800

    Implement tuple

---
+++ Graphics/common/tuple.hpp
@@ -0,0 +1,45 @@
+#pragma once
+
+namespace Graphics {
+	template<unsigned i, typename _Type, typename ...Rest>
+	struct TupleType {
+		using Type = TupleType<i - 1, Rest ...>::Type;
+	};
+
+	template<typename _Type, typename ...Rest>
+	struct TupleType<0, _Type, Rest ...> {
+		using Type = _Type;
+	};
+
+	template<typename ...Types>
+	struct Tuple {
+	};
+
+	template<typename First, typename ...Rest>
+	struct Tuple<First, Rest ...> {
+		using Type = First;
+
+		First value;
+		Tuple<Rest ...> rest;
+
+		Tuple(First value, Rest ...rest) :
+			value(value),
+			rest(Tuple<Rest ...>(rest...)) {
+		}
+
+		template<unsigned i>
+		inline TupleType<i, First, Rest...>::Type &Value() {
+			if constexpr(i == 0U)
+				return value;
+			else
+				return rest.Tuple<Rest ...>::Value<i - 1>();
+		}
+	};
+
+	template<>
+	struct Tuple<> {
+		using Type = void;
+
+		Tuple() = default;
+	};
+}

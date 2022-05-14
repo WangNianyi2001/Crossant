938ffd8924f7e5c202d2dd32ab0d806072bcc585
Nianyi Wang <Wangnianyi2001@outlook.com>
2022-05-15 00:18:36 +0800

    Flythrough scene

--- Main/main.cpp
+++ Main/main.cpp
@@ -42,7 +42,13 @@ int Crossant::Main() {
 		target.DrawOn(window.graphicsTarget);
 	});
 	window.Listen(EventType::Update, [&](WE e) {
-		bool down = Mouse::Pressed(e.mouseButton);
+		using Key = Keyboard::Key;
+		Coord3D camMovement = camera.parent.transform.rotation * Coord3D{
+			(float)Keyboard::Pressed(Key::D) - (float)Keyboard::Pressed(Key::A),
+			(float)Keyboard::Pressed(Key::E) - (float)Keyboard::Pressed(Key::Q),
+			(float)Keyboard::Pressed(Key::S) - (float)Keyboard::Pressed(Key::W),
+		};
+		camera.parent.transform.translation = camera.parent.transform.translation + camMovement * .01f;
 		window.Repaint();
 	});
 	window.Listen(EventType::MouseMove, [&](WE) {
